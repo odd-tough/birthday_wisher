0b68d4e075500c2d91563c62c6aecc5736ceed08
AGENT_CHO <smariyamkt@gmail.com>
2026-08-28 14:46:00 -1200

    Update main.py

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ for i in range(len(row)):
         connection.sendmail(from_addr=my_email, to_addrs="smariy98@yahoo.com",
                             msg=f"subject:Happy Birthday!\n\n{first_line} ")
         connection.close()
-print("EMAIL:", EMAIL)
-print("PASSWORD:", PASSWORD)
+print("EMAIL:", MY_EMAIL)
+print("PASSWORD:", MY_PASSWORD)
         
 
 
